192672690492d172fa8dfac72a1e7338bb38026b
daniel <daniel@mobidoc.us>
2024-09-07 16:48:52 +0900

    first commit

---
+++ src/test/java/org/example/passbatch/PassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.passbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
